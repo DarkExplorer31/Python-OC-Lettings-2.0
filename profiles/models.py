cf0a05dacb5d1e69aa73e4e064616f554194ce61
DarkExplorer31 <drkexplorer@gmal.com>
2024-03-17 16:01:54 +0100

    Finishing the refactoring

--- profiles/models.py
+++ profiles/models.py
@@ -3,8 +3,19 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="++")
+    """
+    Represents a user profile associated with a Django User.
+
+    Attributes:
+        user (OneToOneField): The associated User instance.
+        favorite_city (CharField, optional): The favorite city of the user.
+    """
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     favorite_city = models.CharField(max_length=64, blank=True)
 
     def __str__(self):
+        """
+        Returns a string representation of the profile.
+        """
         return self.user.username
